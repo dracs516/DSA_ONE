275542396a2aeac7e9d56993192e9d1f74f09566
dracs516 <abhayjha.is20@bmsce.ac.in>
2022-11-30 15:45:51 +0530

    sieve of eratosthenes

--- 1_Sieve_Of_Eratosthenes.cpp
+++ 1_Sieve_Of_Eratosthenes.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
 using namespace std;
 
+void sieve_of_eratosthenes(int n, bool primeCheck[])
+{
+    for (int i = 2; i * i <= n; i++)
+    {
+        for (int j = 2 * i; j <= n; j += i)
+        {
+            primeCheck[j] = false;
+        }
+    }
+}
+
 int main()
 {
-    
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    bool primeCheck[n + 1];
+    memset(primeCheck, true, sizeof(primeCheck));
+  
+    primeCheck[0] = false;
+    primeCheck[1] = false;
+
+    sieve_of_eratosthenes(n, primeCheck);
+
+    for (int i = 0; i <= n; i++)
+    {
+        cout << endl
+             << i << " " << primeCheck[i];
+    }
     return 0;
 }
