1f060ea46398a06dc9979ba52cfa3783500d609c
dracs516 <abhayjha.is20@bmsce.ac.in>
2022-12-01 22:23:46 +0530

    sudoku solver updated for checking validity of board

--- 10_Sudoku_Solver.cpp
+++ 10_Sudoku_Solver.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void print_board(int board[n][n])
 {
+    cout << endl;
     for (int i = 0; i < n; i++)
     {
         for (int j = 0; j < n; j++)
@@ -77,6 +78,74 @@ bool sudoku_solver(int board[n][n])
     return false;
 }
 
+bool check_validity(int board[9][9], int row, int col)
+{
+    int i, j;
+    // col
+    for (j = 0; j < 9; j++)
+    {
+        if (board[row][col] != 0)
+        {
+            if (j == col)
+                continue;
+            if (board[row][col] == board[row][j])
+            {
+                cout << "Same in col for board[" << row << "][" << col << "]: " << board[row][col] << "with board[" << row << "][" << j << "]" << endl;
+                return false;
+            }
+        }
+    }
+    // row
+    for (i = 0; i < 9; i++)
+    {
+        if (board[row][col] != 0)
+        {
+            if (i == row)
+                continue;
+            if (board[row][col] == board[i][col])
+            {
+                cout << "Same in row for board[" << row << "][" << col << "]: " << board[row][col] << "with board[" << i << "][" << col << "]" << endl;
+                return false;
+            }
+        }
+    }
+    // 3x3 block
+    int blockRow = row - row % 3;
+    int blockCol = col - col % 3;
+    for (i = 0; i < 3; i++)
+    {
+        for (j = 0; j < 3; j++)
+        {
+            if (board[row][col] != 0)
+            {
+                if (row == blockRow + i && col == blockCol + j)
+                    continue;
+                if (i == row || j == col)
+                    continue;
+                if (board[row][col] == board[blockRow + i][blockCol + j])
+                {
+                    cout << "Same in 3x3 block for board[" << row << "][" << col << "]: " << board[row][col] << " with: board[" << blockRow + i << "][" << blockCol + j << "]" << endl;
+                    return false;
+                }
+            }
+        }
+    }
+    return true;
+}
+
+bool is_valid(int board[9][9])
+{
+    for (int i = 0; i < 9; i++)
+    {
+        for (int j = 0; j < 9; j++)
+        {
+            if (!check_validity(board, i, j))
+                return false;
+        }
+    }
+    return true;
+}
+
 int main()
 {
     int board[9][9];
@@ -85,8 +154,14 @@ int main()
         for (int j = 0; j < 9; j++)
             cin >> board[i][j];
 
-    if (sudoku_solver(board))
-        print_board(board);
+    if (is_valid(board))
+    {
+
+        if (sudoku_solver(board))
+            print_board(board);
+        else
+            cout << "No solution exists for the given board" << endl;
+    }
     else
         cout << "No solution exists for the given board" << endl;
     return 0;
