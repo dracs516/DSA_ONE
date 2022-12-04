1cb392ac42f2ebcf3112c45c1101625d88114a52
dracs516 <abhayjha.is20@bmsce.ac.in>
2022-12-04 20:33:44 +0530

    selection sort

---
+++ 18_Selection_Sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void selectionSort(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        int min_index = i;
+        for (int j = i + 1; j < n; j++)
+        {
+            if (arr[j] < arr[min_index])
+                min_index = j;
+        }
+        swap(arr[i], arr[min_index]);
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+    cout << "Original Array:" << endl;
+    printArray(arr, n);
+    selectionSort(arr, n);
+    cout << "Sorted Array: " << endl;
+    printArray(arr, n);
+}
