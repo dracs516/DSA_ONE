5d6bfd3c94f36fcdb519ab240483d60f39e1183d
dracs516 <abhayjha.is20@bmsce.ac.in>
2022-12-04 13:19:34 +0530

    bubble sort

---
+++ 16_Bubble_Sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+using namespace std;
+
+void printArray(int arr[], int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void swap(int *a, int *b)
+{
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - 1 - i; j++)
+        {
+            if (arr[j] > arr[j + 1])
+                swap(&arr[j], &arr[j + 1]);
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+    int arr[n];
+
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Original array: " << endl;
+    printArray(arr, n);
+    bubbleSort(arr, n);
+    cout << "Sorted array: " << endl;
+    printArray(arr, n);
+}
